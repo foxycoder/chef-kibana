7f20120deb090d2ca58849cc29ebff8e0edfaa30
xueshibing <xueshibing@gmail.com>
2014-11-06 09:24:09 +0800

    download source from offical website

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
-default['kibana']['repo'] = "https://github.com/elasticsearch/kibana"
-default['kibana']['branch'] = "master"
-default['kibana']['git']['checkout'] = false
+default['kibana']['version'] = "3.1.1"
+#default['kibana']['repo'] = "https://github.com/elasticsearch/kibana"
+#default['kibana']['branch'] = "master"
+#default['kibana']['git']['checkout'] = false
 default['kibana']['webserver'] = "nginx"
 default['kibana']['installdir'] = "/opt/kibana"
 default['kibana']['es_server'] = "127.0.0.1"
